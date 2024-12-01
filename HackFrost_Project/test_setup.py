d688a100defcb839b42c519036eaf38484225588
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-12-01 13:02:57 +0530

    Create test_setup.py

---
+++ HackFrost_Project/test_setup.py
@@ -0,0 +1,9 @@
+# 1st Step: This file is used to test if your environment is set up properly and if the dataset loads correctly.
+
+import pandas as pd
+
+# Load the dataset
+df = pd.read_csv('detailed_expenses.csv')
+
+# Print the first few rows of the dataset to check
+print(df.head())
